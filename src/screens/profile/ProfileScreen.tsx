e588ef51b45c74acc5e6b185eca8eb1365a2a38a
Gus Vc <guzz.vc@gmail.com>
2025-06-06 14:43:39 -0600

    feat: ProfileScreed and ProfileSectionButton component added

--- src/screens/profile/ProfileScreen.tsx
+++ src/screens/profile/ProfileScreen.tsx
@@ -3,12 +3,29 @@ import React from 'react'
 import AppSafeView from '../../components/views/AppSafeView'
 import HomeHeader from '../../components/headers/HomeHeader'
 import AppText from '../../components/texts/AppText'
+import ProfileSectionButton from '../../components/buttons/ProfileSectionButton'
+import { sharedPaddingHorizontal } from '../../styles/sharedStyles'
+import { s, vs } from 'react-native-size-matters'
 
 const ProfileScreen = () => {
   return (
     <AppSafeView>
       <HomeHeader />
-      <AppText style={{fontSize: 60}}>Profile Screen</AppText>
+      <View style={{ paddingHorizontal: sharedPaddingHorizontal }}>
+        <AppText style={{ fontSize: s(18), marginTop: vs(10) }}>Hello, Gus</AppText>
+        <ProfileSectionButton
+          onPress={() => console.log('Edit Profile Pressed')}
+          title="My Orders"
+        />
+        <ProfileSectionButton
+          onPress={() => console.log('Edit Profile Pressed')}
+          title="Language"
+        />
+        <ProfileSectionButton
+          onPress={() => console.log('Edit Profile Pressed')}
+          title="Logout"
+        />
+      </View>
     </AppSafeView>
   )
 }
